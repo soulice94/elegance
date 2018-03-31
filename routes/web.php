c447683080d3f89cb75eadb3d4f9553ecdb1acc8
Pedro Arturo Flores Ramirez <pedro@MacBook-Pro.local>
2018-03-31 13:54:16 -0600

    Creación de la base para productos

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/productos', array(
+    'as'    => 'productos_index',
+    'uses'  => 'ProductoController@index'
+));
