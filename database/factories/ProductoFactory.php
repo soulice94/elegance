7c5356c6efb41038e7095661d278558e205eca35
Pedro Arturo Flores Ramirez <pedro@MacBook-Pro.local>
2018-04-05 23:32:25 -0500

    Modificación del factory de productos para añadir nulos a descuento

--- database/factories/ProductoFactory.php
+++ database/factories/ProductoFactory.php
@@ -4,19 +4,20 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Producto::class, function (Faker $faker) {
     $costo = $faker->numberBetween(1000, 10000);
+    $descuento = $faker->boolean ? $faker->randomDigitNotNull : null;
     return [
         //
         'nombre'        => $faker->sentence(4),
         'costo'         => $costo,
         'precio'        => $costo+$faker->numberBetween(1000, 3000),
         'unidades'      => $faker->randomDigitNotNull,
-        'descuento'     => $faker->randomDigit,
+        'descuento'     => $descuento,
         'descripcion'   => $faker->sentence(8),
         'modelo'        => $faker->isbn10,
         'codigo'        => $faker->bothify('???###??##'),
         'categorias_ID' => $faker->numberBetween(1,10),
         'marcas_ID'     => $faker->numberBetween(1,10),
-        'colors_ID'     => $faker->numberBetween(1,3),
+        'colors_ID'     => $faker->numberBetween(1,4),
         'generos_ID'    => $faker->numberBetween(1,3)
     ];
 });
